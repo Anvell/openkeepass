df1634f416eb6f11e18999b2408ec8d741191d67
Denis Trotsenko <denis.tnko@gmail.com>
2019-07-23 22:49:33 +0200

    Allow boolean values from xml to be null

--- src/main/java/de/slackspace/openkeepass/domain/xml/adapter/BooleanSimpleXmlAdapter.java
+++ src/main/java/de/slackspace/openkeepass/domain/xml/adapter/BooleanSimpleXmlAdapter.java
@@ -6,12 +6,21 @@ public class BooleanSimpleXmlAdapter implements Transform<Boolean> {
 
     @Override
     public Boolean read(String value) throws Exception {
-        return "true".equalsIgnoreCase(value);
+        switch (value.toLowerCase()) {
+            case "true":
+                return true;
+            case "false":
+                return false;
+            default:
+                return null;
+        }
     }
 
     @Override
     public String write(Boolean value) throws Exception {
-        if (value != null && value) {
+        if(value == null) {
+            return "null";
+        } else if (value) {
             return "True";
         }
         return "False";
